d4de8044a94cb5642efea8b26fe249bf83565cd5
Nicolas Boullon <boullon.nicolas@gmail.com>
2024-11-23 19:24:53 -0300

    corrigiendo pipe buscar mis turnos como especialista

--- src/app/core/pipes/buscar-paciente-especialidad.pipe.ts
+++ src/app/core/pipes/buscar-paciente-especialidad.pipe.ts
@@ -1,3 +1,4 @@
+import { formatDate } from '@angular/common';
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
@@ -11,9 +12,43 @@ export class BuscarPacienteEspecialidadPipe implements PipeTransform {
       return turnos;
     }
 
-    return turnos.filter((turno)=> turno.especialidad.toLowerCase().includes(buscar.toLowerCase()) ||
-     turno.paciente.nombre.toLowerCase().includes(buscar.toLowerCase()) ||
-      turno.paciente.apellido.toLowerCase().includes(buscar.toLowerCase()));
+    return turnos.filter((turno)=>{
+      const fecha = new Date(turno.dia.seconds * 1000);
+      const fechaFormateada = formatDate(fecha, 'dd MMMM yyyy', 'es-AR');
+
+      if(turno.historialClinico){
+        return (
+          turno.especialidad.toLowerCase().includes(buscar.toLowerCase()) ||
+          turno.paciente.nombre.toLowerCase().includes(buscar.toLowerCase()) ||
+          turno.estado.toLowerCase().includes(buscar.toLowerCase()) ||
+          turno.horario.toLowerCase().includes(buscar.toLowerCase()) ||
+          turno.historialClinico.presion.toLowerCase().includes(buscar.toLowerCase())  ||
+          turno.historialClinico.altura.toString().toLowerCase().includes(buscar.toLowerCase())  ||
+          turno.historialClinico.peso.toString().toLowerCase().includes(buscar.toLowerCase())  ||
+          turno.historialClinico.temperatura.toString().toLowerCase().includes(buscar.toLowerCase())  ||
+          Object.keys(turno.historialClinico.campoDinamicoUno).toString().toLowerCase().includes(buscar.toLowerCase())  ||//din
+          Object.values(turno.historialClinico.campoDinamicoUno).toString().toLowerCase().includes(buscar.toLowerCase())  ||//din
+          Object.keys(turno.historialClinico.campoDinamicoDos).toString().toLowerCase().includes(buscar.toLowerCase())  ||//din
+          Object.values(turno.historialClinico.campoDinamicoDos).toString().toLowerCase().includes(buscar.toLowerCase())  ||//din
+          Object.keys(turno.historialClinico.campoDinamicoTres).toString().toLowerCase().includes(buscar.toLowerCase())  ||//din
+          Object.values(turno.historialClinico.campoDinamicoTres).toString().toLowerCase().includes(buscar.toLowerCase())  ||//din
+          fechaFormateada.toLowerCase().includes(buscar.toLowerCase()) ||
+          turno.paciente.apellido.toLowerCase().includes(buscar.toLowerCase())
+        );
+      }else{
+
+        return (
+          turno.especialidad.toLowerCase().includes(buscar.toLowerCase()) ||
+          turno.paciente.nombre.toLowerCase().includes(buscar.toLowerCase()) ||
+          turno.estado.toLowerCase().includes(buscar.toLowerCase()) ||
+          turno.horario.toLowerCase().includes(buscar.toLowerCase()) ||
+          // turno.historialClinico.presion? (turno.historialClinico.presion.toLowerCase().includes(buscar.toLowerCase())): false  ||
+          fechaFormateada.toLowerCase().includes(buscar.toLowerCase()) ||
+          turno.paciente.apellido.toLowerCase().includes(buscar.toLowerCase())
+        );
+      }
+      
+    })
   }
 
 }
